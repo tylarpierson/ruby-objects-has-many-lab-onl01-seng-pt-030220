bd641e963e5143dd4a8f7a93d19db0729394159c
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-01 15:43:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,11 @@
 class Artist 
-  attr_accessor :name, :songs
+  attr_accessor :name
   @@song_count = 0 
   def initialize(name)
     @name = name
-    @songs = []
+    
   end 
   def add_song(song)
-    @songs << song 
     song.artist = self 
     @@song_count += 1 
   end 
