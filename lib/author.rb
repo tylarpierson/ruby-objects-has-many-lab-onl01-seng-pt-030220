316c7481a487f58a73c69a8898fde040fa8de164
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-01 16:00:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,6 @@
 class Author 
-  attr_accessor :name 
+  attr_accessor :name
+  @@post_count = 0 
   def initialize(name)
     @name = name 
     @posts = [] 
@@ -9,9 +10,14 @@ class Author
   end 
   def add_post(post) 
     post.author = self 
+    @@post_count += 1 
   end 
   def add_post_by_title(title) 
     post = Post.new(title)
     add_post(post)
+    @@post_count += 1 
+  end 
+  def post_count 
+    @@post_count
   end 
 end 
