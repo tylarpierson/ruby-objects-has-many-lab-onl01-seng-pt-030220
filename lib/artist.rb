9a9543f4700973430aed6caedfb8ebcddbcf32c0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-01 15:41:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,8 @@ class Artist
   end 
   def add_song_by_name(name)
     song = Song.new(name)
-    add_song(song)
+    @songs << song
+    song.artist = self
     song_count += 1
   end 
   def self.song_count 
