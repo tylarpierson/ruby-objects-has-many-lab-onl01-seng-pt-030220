0bded95f9d9afa35ddddcbb7630fa8513845f714
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-01 15:07:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,11 @@ class Artist
   attr_accessor :name
   def initialize(name)
     @name = name
-    @songs = []
   end 
   def add_song(song)
-    @songs << song 
     song.artist = self 
   end 
   def songs 
-    @songs 
+    Song.all.select {|song| song.artist == self} 
   end 
 end 
